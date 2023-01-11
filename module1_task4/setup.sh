73eabf729c6462ae670bb22b41d26fc85cdb5c76
Caballero018 <alejoxcaballero18@gmail.com>
2023-01-11 18:22:46 -0500

    Shell script named setup.sh to reproduce a pseudo “production” environment locally

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 apt-get update
-apt install hugo
-apt install make
+apt-get install -y hugo make wget
+wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb
+apt install ./hugo_0.84.0_Linux-64bit.deb
 make build
